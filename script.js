b733ffb6b9e07326fee7fd1110d03d73766fe919
Steven Nguyen <105184434+SdbnCode@users.noreply.github.com>
2024-02-15 22:38:43 -0500

    Got the stats to show up but the name does not appear and duplicates

--- script.js
+++ script.js
@@ -1,55 +1,54 @@
 const editIcon = `<i class="fas fa-edit"></i>` 
-
 const deleteIcon = `<i class="fas fa-trash"></i>` 
 
 function clearInput() { 
-  NameInput.value = ""
-  positionInput.checked = false
-  gameInput.value = ""
-  pointsInput.value = ""
-  assistsInput.value = ""
-  reboundsInput.value = ""
-  blocksInput.value = ""
-  stealsInput.value = ""
-  freeThrowsInput.value = ""
-  freeThrowsMadeInput.value = ""
-  twoPointersInput.value = ""
-  threePointersInput.value = ""
+  pNameInput.value = ""
+  pPositionInput.checked = false
+  pGameInput.value = ""
+  pPointsInput.value = ""
+  pAssistsInput.value = ""
+  pReboundsInput.value = ""
+  pBlocksInput.value = ""
+  pStealsInput.value = ""
+  pFreeThrowsInput.value = ""
+  pFreeThrowsMadeInput.value = ""
+  pTwoPointersInput.value = ""
+  pThreePointersInput.value = ""
 } 
 
 function LocalStorage(){ 
-  localStorage.setItem("name", JSON.stringify(NAME));
-  localStorage.setItem("position", JSON.stringify(POSITION));
-  localStorage.setItem("game", JSON.stringify(GAME));
-  localStorage.setItem("points", JSON.stringify(POINTS));
-  localStorage.setItem("assists", JSON.stringify(ASSISTS));
-  localStorage.setItem("rebounds", JSON.stringify(REBOUNDS));
-  localStorage.setItem("blocks", JSON.stringify(BLOCKS));
-  localStorage.setItem("steals", JSON.stringify(STEALS));
-  localStorage.setItem("freeThrows", JSON.stringify(FREETHROWS));
-  localStorage.setItem("freeThrowsMade", JSON.stringify(FREETHROWSMADE));
-  localStorage.setItem("twoPointers", JSON.stringify(TWOPOINTERS));
-  localStorage.setItem("threePointers", JSON.stringify(THREEPOINTERS));
+  localStorage.setItem("playerName", JSON.stringify(playerName));
+  localStorage.setItem("position", JSON.stringify(position));
+  localStorage.setItem("game", JSON.stringify(game));
+  localStorage.setItem("points", JSON.stringify(points));
+  localStorage.setItem("assists", JSON.stringify(assists));
+  localStorage.setItem("rebounds", JSON.stringify(rebounds));
+  localStorage.setItem("blocks", JSON.stringify(blocks));
+  localStorage.setItem("steals", JSON.stringify(steals));
+  localStorage.setItem("freeThrows", JSON.stringify(freeThrows));
+  localStorage.setItem("freeThrowsMade", JSON.stringify(freeThrowsMade));
+  localStorage.setItem("twoPointers", JSON.stringify(twoPointers));
+  localStorage.setItem("threePointers", JSON.stringify(threePointers));
 }
 
 function addPlayer(){ 
   const tbody = document.getElementById("output");
-  const rows = Math.max(NAME.length, POSITION.length, GAME.length, POINTS.length, ASSISTS.length, REBOUNDS.length, BLOCKS.length, STEALS.length, FREETHROWS.length, FREETHROWSMADE.length, TWOPOINTERS.length, THREEPOINTERS.length);
+  const rows = Math.max(playerName.length, position.length, game.length, points.length, assists.length, rebounds.length, blocks.length, steals.length, freeThrows.length, freeThrowsMade.length, twoPointers.length, threePointers.length)
   let html = "";
 
   for(let i=0; i<rows; i++){ 
-    let pName = NAME[i] || "N/A";
-    let pPosition = POSITION[i] || "N/A";
-    let pGame = GAME[i] || "N/A";
-    let pPoints = POINTS[i] || "N/A";
-    let pAssists = ASSISTS[i] || "N/A";
-    let pRebounds = REBOUNDS[i] || "N/A";
-    let pBlocks = BLOCKS[i] || "N/A";
-    let pSteals = STEALS[i] || "N/A";
-    let pFreeThrows = FREETHROWS[i] || "N/A";
-    let pFreeThrowsMade = FREETHROWSMADE[i] || "N/A";
-    let pTwoPointers = TWOPOINTERS[i] || "N/A";
-    let pThreePointers = THREEPOINTERS[i] || "N/A";
+    let pName = playerName[i] || "N/A";
+    let pPosition = position[i] || "N/A";
+    let pGame = game[i] || "N/A";
+    let pPoints = points[i] || "N/A";
+    let pAssists = assists[i] || "N/A";
+    let pRebounds = rebounds[i] || "N/A";
+    let pBlocks = blocks[i] || "N/A";
+    let pSteals = steals[i] || "N/A";
+    let pFreeThrows = freeThrows[i] || "N/A";
+    let pFreeThrowsMade = freeThrowsMade[i] || "N/A";
+    let pTwoPointers = twoPointers[i] || "N/A";
+    let pThreePointers = threePointers[i] || "N/A";
     html += `<tr> 
         <td>${pName}</td> 
         <td>${pPosition}</td>
@@ -74,84 +73,85 @@ function addPlayer(){
   tbody.innerHTML = html; 
 } 
 
-let editIndex = -1; 
+// let editIndex = -1; 
 
-let NAME = JSON.parse(localStorage.getItem("name")) || [];
-let POSITION = JSON.parse(localStorage.getItem("position")) || [];
-let GAME = JSON.parse(localStorage.getItem("game")) || [];
-let POINTS = JSON.parse(localStorage.getItem("points")) || [];
-let ASSISTS = JSON.parse(localStorage.getItem("assists")) || [];
-let REBOUNDS = JSON.parse(localStorage.getItem("rebounds")) || [];
-let BLOCKS = JSON.parse(localStorage.getItem("blocks")) || [];
-let STEALS = JSON.parse(localStorage.getItem("steals")) || [];
-let FREETHROWS = JSON.parse(localStorage.getItem("freeThrows")) || [];
-let FREETHROWSMADE = JSON.parse(localStorage.getItem("freeThrowsMade")) || [];
-let TWOPOINTERS = JSON.parse(localStorage.getItem("twoPointers")) || [];
-let THREEPOINTERS = JSON.parse(localStorage.getItem("threePointers")) || [];
+let playerName = JSON.parse(localStorage.getItem("playerName")) || [];
+let position = JSON.parse(localStorage.getItem("position")) || [];
+let game = JSON.parse(localStorage.getItem("game")) || [];
+let points = JSON.parse(localStorage.getItem("points")) || [];
+let assists = JSON.parse(localStorage.getItem("assists")) || [];
+let rebounds = JSON.parse(localStorage.getItem("rebounds")) || [];
+let blocks = JSON.parse(localStorage.getItem("blocks")) || [];
+let steals = JSON.parse(localStorage.getItem("steals")) || [];
+let freeThrows = JSON.parse(localStorage.getItem("freeThrows")) || [];
+let freeThrowsMade = JSON.parse(localStorage.getItem("freeThrowsMade")) || [];
+let twoPointers = JSON.parse(localStorage.getItem("twoPointers")) || [];
+let threePointers = JSON.parse(localStorage.getItem("threePointers")) || [];
 
 
-const NameInput = document.getElementById("name")
-const positionInput = document.getElementById("position")
-const gameInput = document.getElementById("game")
-const pointsInput = document.getElementById("points")
-const assistsInput = document.getElementById("assists")
-const reboundsInput = document.getElementById("rebounds")
-const blocksInput = document.getElementById("blocks")
-const stealsInput = document.getElementById("steals")
-const freeThrowsInput = document.getElementById("freeThrows")
-const freeThrowsMadeInput = document.getElementById("freeThrowsMade")
-const twoPointersInput = document.getElementById("twoPointers")
-const threePointersInput = document.getElementById("threePointers")
+const pNameInput = document.getElementById("playerName")
+const pPositionInput = document.getElementById("position")
+const pGameInput = document.getElementById("game")
+const pPointsInput = document.getElementById("points")
+const pAssistsInput = document.getElementById("assists")
+const pReboundsInput = document.getElementById("rebounds")
+const pBlocksInput = document.getElementById("blocks")
+const pStealsInput = document.getElementById("steals")
+const pFreeThrowsInput = document.getElementById("freeThrows")
+const pFreeThrowsMadeInput = document.getElementById("freeThrowsMade")
+const pTwoPointersInput = document.getElementById("twoPointers")
+const pThreePointersInput = document.getElementById("threePointers")
 
 const submitButton = document.getElementById("submit")
-const editSection = document.getElementById("editSection")
+// const editSection = document.getElementById("editSection")
 
 addPlayer()
 
+//DONE
 submitButton.addEventListener("click", ()=>{ 
-  const name = NameInput.value || null;
-  const position = positionInput.value || null;
-  const game = gameInput.value || null;
-  const points = pointsInput.value || null;
-  const assists = assistsInput.value || null;
-  const rebounds = reboundsInput.value || null;
-  const blocks = blocksInput.value || null;
-  const steals = stealsInput.value || null;
-  const freeThrows = freeThrowsInput.value || null;
-  const freeThrowsMade = freeThrowsMadeInput.value || null;
-  const twoPointers = twoPointersInput.value || null;
-  const threePointers = threePointersInput.value || null;
+  const pName = pNameInput.value || null;
+  const pPosition = pPositionInput.value || null;
+  const pGame = pGameInput.value || null;
+  const pPoints = pPointsInput.value || null;
+  const pAssists = pAssistsInput.value || null;
+  const pRebounds = pReboundsInput.value || null;
+  const pBlocks = pBlocksInput.value || null;
+  const pSteals = pStealsInput.value || null;
+  const pFreeThrows = pFreeThrowsInput.value || null;
+  const pFreeThrowsMade = pFreeThrowsMadeInput.value || null;
+  const pTwoPointers = pTwoPointersInput.value || null;
+  const pThreePointers = pThreePointersInput.value || null;
 
-  if(name === null || position === null || game === null || points === null || assists === null || rebounds === null || blocks === null || steals === null || freeThrows === null || freeThrowsMade === null || twoPointers === null || threePointers === null) { 
-    alert("Please enter all the fields.") 
+if(!pName === null || !pPosition === null || !pGame === null || !pPoints === null || !pAssists === null || !pRebounds === null || !pBlocks === null || !pSteals === null || !pFreeThrows === null || !pFreeThrowsMade === null || !pTwoPointers === null || !pThreePointers === null){
+  alert("Please enter all the fields.") 
     return
   } 
 
- NAME = [name, ...NAME];
- POSITION = [position, ...POSITION];
- GAME = [game, ...GAME];
- POINTS = [points, ...POINTS];
- ASSISTS = [assists, ...ASSISTS];
- REBOUNDS = [rebounds, ...REBOUNDS];
- BLOCKS = [blocks, ...BLOCKS];
- STEALS = [steals, ...STEALS];
- FREETHROWS = [freeThrows, ...FREETHROWS];
- FREETHROWSMADE = [freeThrowsMade, ...FREETHROWSMADE];
- TWOPOINTERS = [twoPointers, ...TWOPOINTERS];
- THREEPOINTERS = [threePointers, ...THREEPOINTERS];
+playerName = [pName, ...playerName];
+position = [pPosition, ...position];
+game = [pGame, ...game];
+points = [pPoints, ...points];
+assists = [pAssists, ...assists];
+rebounds = [pRebounds, ...rebounds];
+blocks = [pBlocks, ...blocks];
+steals = [pSteals, ...steals];
+freeThrows = [pFreeThrows, ...freeThrows];
+freeThrowsMade = [pFreeThrowsMade, ...freeThrowsMade];
+twoPointers = [pTwoPointers, ...twoPointers];
+threePointers = [pThreePointers, ...threePointers];
 
- NAME.push(name);
- POSITION.push(position);
- GAME.push(game);
- POINTS.push(points);
- ASSISTS.push(assists);
- REBOUNDS.push(rebounds);
- BLOCKS.push(blocks);
- STEALS.push(steals);
- FREETHROWS.push(freeThrows);
- FREETHROWSMADE.push(freeThrowsMade);
- TWOPOINTERS.push(twoPointers);
- THREEPOINTERS.push(threePointers);
+playerName.push(pName);
+position.push(pPosition);
+game.push(pGame);
+points.push(pPoints);
+assists.push(pAssists);
+rebounds.push(pRebounds);
+blocks.push(pBlocks);
+steals.push(pSteals);
+freeThrows.push(pFreeThrows);
+freeThrowsMade.push(pFreeThrowsMade);
+twoPointers.push(pTwoPointers);
+threePointers.push(pThreePointers);
   
  clearInput(); 
  addPlayer();
